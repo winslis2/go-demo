ce1156bdb68447e3d7162f7208cf96748b3195ec
winslis2 <kunmingqaq@163.com>
2021-02-04 10:27:13 +0800

    slice 初始化和make的区别

--- slice.go
+++ slice.go
@@ -3,17 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	//slice_test()
-	slice_copy()
+	slice_test()
+	//slice_copy()
 }
 func slice_test() {
 	//cap如果小len会直接报错
-	s := make([]int, 2, 3) //超出容量也没事，会自动扩容
+	s := make([]int, 2, 3) //超出容量也没事，会自动扩容   这种情况前两个值是0，append 追加的是从index=2开始的
+	//如果需要切面初始化	s:=int[]{1,2,3}
 	s = append(s, 1)
 	s = append(s, 2)
-	nums := s[0:2]
-	fmt.Println(s)
-	fmt.Println(nums)
+	for k, v := range s {
+		fmt.Println(k, v)
+	}
 }
 
 func slice_copy() {
